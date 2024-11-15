b251ae617ecbe5199285e8e249baea7176cd4d96
Cesya <dealyalti_2226240037@mhs.mdp.ac.id>
2024-11-15 18:30:10 +0700

    commit

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,5 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { HousingLocation } from '../housing-location';
+import { HousingService } from '../housing.service';
 
 @Component({
   selector: 'app-details',
@@ -15,8 +17,12 @@ import { ActivatedRoute } from '@angular/router';
 export class DetailsComponent {
   route : ActivatedRoute = inject(ActivatedRoute);
   housingLocationId = 0;
+  housingService : HousingService = inject(HousingService);
+  housingLocation : HousingLocation | undefined
 
   constructor(){
     this.housingLocationId = Number(this.route.snapshot.params['id'])
+    this.housingLocation = this.housingService.getHousingLocationById(this.housingLocationId)
+    console.table(this.housingLocation)
   }
 }
